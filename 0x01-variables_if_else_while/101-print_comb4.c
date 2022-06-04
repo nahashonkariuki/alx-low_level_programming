5bcf6508461c7025865a82bf5b2738d4fc8c69c6
nahashonkariuki <n.kariuki@alustudent.com>
2022-06-03 20:18:49 -0700

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,39 @@
-i will
+#include <stdio.h>
+
+/**
+ * main - Print combinations of three digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int hundreds;
+int tens;
+int ones;
+
+for (hundreds = 0; hundreds <= 9; hundreds++)
+	{
+	for (tens = 0; tens <= 9; tens++)
+		{
+		putchar(hundreds + '0');
+		if (hundreds < 8)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		for (ones = tens + 1; ones <= 9; ones++)
+			{
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (tens < 8)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+}
