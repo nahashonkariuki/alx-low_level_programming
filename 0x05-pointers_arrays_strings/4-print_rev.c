657833ac345a3c4926ce3e3d9325dd0fe46293f9
Nahashon Kariuki <nash.mwangi.kariuki@gmail.com>
2022-06-14 06:09:57 +0300

    5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen -  swaps the values of two integers.
+ * @s: String input
+ *
+ * Return: Lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_rev - Printsca string in reverse
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = _strlen(s) - 1;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
